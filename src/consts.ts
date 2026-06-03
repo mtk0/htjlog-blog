d0f6ce9f5856c28ebd18bcfe9ba91faccbc43359
mtk0 <matsuk@matsukennoMacBook-Air.local>
2026-06-03 10:55:59 +0900

    サイトタイトル変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'ヒツジの記録 | 50代フリーランスの美容・写真・生き方';
+export const SITE_TITLE = '遠回りしてきた50代フリーランスの記録';
 export const SITE_DESCRIPTION = '美容・写真を中心に20年以上フリーランスとして活動するヒツジが、50代からの働き方・お金・健康・趣味について発信するブログです。';
